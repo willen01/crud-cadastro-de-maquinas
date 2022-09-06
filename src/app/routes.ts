2492368e130770037139797fb6f4b26dc7336c9e
Willen dos Santos <95586829+willen01@users.noreply.github.com>
2022-09-06 14:34:20 -0300

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import UserController from "../domain/controllers/UserController";
+import UserController from "../domain/controllers/MachineController";
 
 const router = Router();
 
