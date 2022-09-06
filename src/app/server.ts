9ccd95d26ca07ef68944b5d2f44c391df2c5550c
willen <willen.sesantos@gmail.com>
2022-09-06 15:06:06 -0300

    chore: Atribui porta de conexão a variável de ambiente

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import DbConnection from "../domain/repositories/DbConnection";
 import { router } from "./routes";
+import "dotenv/config";
 
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
 app.use(router);
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-    DbConnection.connect();
-})
+  console.log(`Servidor rodando na porta ${PORT}`);
+  DbConnection.connect();
+});
